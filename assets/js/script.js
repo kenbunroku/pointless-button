af8afbebf9ec032120d455b1b4287ad954d2d855
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2020-09-17 22:02:53 -0500

    start

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// GET PRESS BUTTON
+let button = document.querySelector('.button'),
+  count = 0;
+
+// ADD EVENT WHEN THE BUTTON IS PRESSED
+button.addEventListener('click', changeNumber);
+
+function changeNumber() {
+  // INCLEMENT THE NUMBER
+  count += 1;
+
+  // DISPLAY THE NUMBER IN HTML
+  button.innerHTML = count;
+}
